ce36021fd46bb038588c30b2e3b2f4528f82d11b
TaoZi-hub <3149233223@qq.com>
2021-06-11 23:22:52 +0800

    项目自身的配置文件第三次

--- demo-modul-1/src/com/duyi/MainTest2.java
+++ demo-modul-1/src/com/duyi/MainTest2.java
@@ -5,5 +5,6 @@ public class MainTest2 {
     public static void main(String[] args) {
         System.out.println();
         System.out.println("求求你了成功吧");
+        System.out.println("");
     }
 }
