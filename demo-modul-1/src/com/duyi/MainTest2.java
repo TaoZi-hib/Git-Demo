d01a5099b3a1ac0e0aa186480ecac64d2e483e6f
TaoZi-hub <3149233223@qq.com>
2021-06-12 00:00:33 +0800

    项目自身的配置文件第16次

--- demo-modul-1/src/com/duyi/MainTest2.java
+++ demo-modul-1/src/com/duyi/MainTest2.java
@@ -7,7 +7,5 @@ public class MainTest2 {
         System.out.println("求求你了成功吧");
         System.out.println("");
         System.out.println("接着来");
-        System.out.println("真一群傻吊");
-        System.out.println("这");
     }
 }
